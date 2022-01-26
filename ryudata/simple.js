cf2588ced25d28fae63066e07d124b3ff0088419
MAUL BOTZ <97677152+AzenMKS@users.noreply.github.com>
2022-01-26 11:26:06 +0700

    Add files via upload

---
+++ ryudata/simple.js
@@ -0,0 +1,18 @@
+exports.smenu = (pushname, namabot) => {
+	return`Halo Kak ${pushname} 👋
+Saya ${namabot} Siap Melayani Anda, Bot Ini Memakai Program
+Multi Device Beta Dari WhatsApp Messenger.
+
+*LINK YT MAUL BOTZ :*
+https://youtube.com/channel/UC6-SaCfnLhy_gHdBodNFP1A
+
+*LINK GROUP MAUL BOTZ :*
+https://chat.whatsapp.com/HpHQiTyXxncBxtxHa1j0lC
+
+Jangan Lupa Subscribe Chennel Owner Ku Yah
+Jadi Jika Menemukan Bug Silahkan Laporkan Kepada Owner
+Atau Creator Agar Segera Di Fix 🤗
+
+Terima Kasih
+`
+}
