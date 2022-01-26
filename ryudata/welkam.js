22d0467a3a995d945870e670ec15ea52e5b7c11a
MAUL BOTZ <97677152+AzenMKS@users.noreply.github.com>
2022-01-26 11:51:22 +0700

    Add files via upload

---
+++ ryudata/welkam.js
@@ -0,0 +1,28 @@
+//EDIT SESUKA KALIAN 😉
+
+// UCAPAN WELCOME KAK
+exports.welkam = () => {
+	return `Intro Dulu Yuk Biar Keren 🤙
+
+📛 Nama : 
+🔞 Umur :
+🍂 Gender :
+🏙️ Askot :
+
+Semoga Kamu Senang Berada Disini
+Serta Jangan Lupa Untuk Membaca
+Dan Mematuhi Rules Yang Ada
+*Subscribe To Yt Owner Ku😣*
+https://youtube.com/channel/UC6-SaCfnLhy_gHdBodNFP1A
+`
+}
+
+// UCAPAN GOODBYE KAK
+exports.goodbye = () => {
+	return `Tetap Putus Asa Jangan
+Semangat Dan Jadilah Beban
+Keluarga 😌🍂
+*Subscribe Bye Yt Owner Ku*😣
+https://youtube.com/channel/UC6-SaCfnLhy_gHdBodNFP1A
+`
+}
