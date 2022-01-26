1fdb67a31c40e23452da456a3ff312e6c5852385
MAUL BOTZ <97677152+AzenMKS@users.noreply.github.com>
2022-01-26 11:25:13 +0700

    Add files via upload

---
+++ ryudata/sewa.js
@@ -0,0 +1,31 @@
+exports.sewa = () => {
+	return`🤖🇸​🇪​🇼​🇦​ 🇧​🇴​🇹​ 🇼​🇦​🤖
+
+    [  *BY AZEN STORE* ]
+
+➢ *𝐏𝐄𝐑𝐌𝐀𝐍𝐄𝐍 : 6𝐊*
+
+𓆉      𝑭𝑰𝑻𝑼𝑹 𝑩𝑶𝑻     𓆉
+
+➵ *WELCOME IMAGE*
+➵ *ANTI LINK ON*
+➵ *ANTI VIRTEX ON*
+➵ *ANTI WAME ON*
+➵  *KICK & ADD*
+➵ *STIKER*
+➵ *HIDETAG*
+
+💸 *𝗣𝗔𝗬𝗠𝗘𝗡𝗧* 💸 
+*DANA*
+*GOPAY*
+*QRIS PAY*
+
+
+➢  *TESTI?BANYAK*
+
+*SISTEM: TES BOT-MASUKIN BOT-TF-DONE*
+
+*MINAT? CHAT :
+wa.me/62895416489526
+`
+}
